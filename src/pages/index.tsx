d9925e2994c44da4830ad7d5fed8a4d742536357
Andrew Ghostuhin <me@torinasakura.name>
2024-11-06 22:12:07 +0300

    Delete src/pages/index.tsx
    Signed-off-by: Andrew Ghostuhin <me@torinasakura.name>

--- src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Redirect } from '@docusaurus/router';
-import { useBaseUrl } from '@docusaurus/core';
-
-export default function Home(): JSX.Element {
-  const baseUrl = useBaseUrl('/gdd/introduction');
-  return <Redirect to={baseUrl} />;
-}
