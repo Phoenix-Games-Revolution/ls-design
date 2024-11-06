20fc245a7a0c3ba10848c14f8b0eecc7a5d56517
Andrew Ghostuhin <me@torinasakura.name>
2024-11-06 20:55:48 +0300

    feat(docs): init docusaurus

---
+++ docusaurus.config.ts
@@ -0,0 +1,90 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Last Strategy',
+  tagline: 'by Phoenix Games Revolution',
+  favicon: 'img/favicon.ico',
+
+  url: 'https://phoenix-games-revolution.github.io',
+  baseUrl: '/',
+
+  organizationName: 'phoenix-games-revolution',
+  projectName: 'last-strategy-docs',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'ru',
+    locales: ['ru', 'en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          path: 'docs',
+          routeBasePath: '/',
+          sidebarPath: './sidebars.ts',
+          editUrl: 'https://github.com/phoenix-games-revolution/last-strategy-docs/edit/master/',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Last Strategy',
+      logo: {
+        alt: 'Phoenix Games Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'docsSidebar',
+          position: 'left',
+          label: 'Documentation',
+        },
+        {
+          href: 'https://github.com/phoenix-games-revolution/last-strategy-docs',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Game Design',
+          items: [
+            {
+              label: 'Об игре',
+              to: '/gdd/introduction',
+            },
+            {
+              label: 'Цели',
+              to: '/gdd/objectives',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Phoenix Games Revolution`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
